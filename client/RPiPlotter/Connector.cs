e712eb935ac481a60af7216b523b153c15d04560
Adam Rutkowski <a_rutkowski@outlook.com>
2015-02-27 18:15:56 +0100

    Initial commit

---
+++ client/RPiPlotter/Connector.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace RPiPlotter
+{
+	public class Connector
+	{
+		public Gtk.Window Parent { get; set; }
+
+		public string Hostname { get; set; }
+
+		public int Port { get; set; }
+
+		public event EventHandler Connected;
+		public event UnhandledExceptionEventHandler ConnectionError;
+
+
+		TcpClient client;
+		Thread receiverThread;
+
+		public Connector(Gtk.Window parent)
+		{
+			Parent = parent;
+		}
+
+		public Connector(Gtk.Window parent, string hostname, int port)
+			: this (parent)
+		{
+			Hostname = hostname;
+			Port = port;
+		}
+
+		public void Connect()
+		{
+			client = new TcpClient ();
+			var result = client.BeginConnect(Hostname, Port, null, null);
+
+			var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(1));
+
+			if (!success)
+			{
+				throw new Exception("Failed to connect.");
+			}
+
+			// we have connected
+			client.EndConnect(result);
+
+			receiverThread = new Thread (() => {
+				var stream = client.GetStream ();
+				while (receiverThread.ThreadState != ThreadState.AbortRequested) {
+					if (stream.DataAvailable) {
+						var readBuffer = new byte[1024];
+						int bytesCount = stream.Read (readBuffer, 0, readBuffer.Length);
+						string msg = Encoding.UTF8.GetString (readBuffer, 0, bytesCount);
+						Gtk.Application.Invoke ((sender, e) => {
+							var dialog = new Gtk.MessageDialog (Parent, Gtk.DialogFlags.DestroyWithParent, Gtk.MessageType.Info, Gtk.ButtonsType.Ok, msg);
+							dialog.Run ();
+							dialog.Destroy ();
+						});
+					}
+					Thread.Sleep (10);
+				}
+			});
+			receiverThread.Start ();
+			if (Connected != null)
+				Connected (this, new EventArgs ());
+		}
+
+		public void Disconnect()
+		{
+			receiverThread.Abort ();
+			client.Close ();
+		}
+
+		public void Send()
+		{
+			try {
+
+			} catch (Exception ex) {
+
+			}
+		}
+	}
+}
+
---
+++ client/RPiPlotter/MainWindow.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Gtk;
+
+namespace RPiPlotter
+{
+	public partial class MainWindow : Gtk.Window
+	{
+		ListStore commandListStore;
+		Connector connector;
+
+		public MainWindow() :
+			base (WindowType.Toplevel)
+		{
+			this.Build ();
+			this.SetupCommandTreeView ();
+		}
+
+		void SetupCommandTreeView()
+		{
+			TreeViewColumn statusColumn = new TreeViewColumn ();
+			statusColumn.Title = "Status";
+
+			TreeViewColumn commandColumn = new TreeViewColumn ();
+			commandColumn.Title = "Command";
+
+			// Add the columns to the TreeView
+			commandTreeView.AppendColumn (statusColumn);
+			commandTreeView.AppendColumn (commandColumn);
+
+			commandListStore = new ListStore (typeof(Image), typeof(string));
+			commandTreeView.Model = commandListStore;
+		}
+
+		void OnQuit(object o, EventArgs args)
+		{
+			if (connector != null)
+				connector.Disconnect ();
+			Application.Quit ();
+		}
+
+		void OnConnectActionActivated(object sender, EventArgs e)
+		{
+			ConnectDialog connectDialog = new ConnectDialog (this);
+			connectDialog.Response += (o, args) => {
+				if (args.ResponseId != ResponseType.Ok)
+					return;
+				connector = new Connector (this, connectDialog.Hostname, connectDialog.Port);
+				connector.Connected += OnConnectorConnected;
+				connector.ConnectionError += OnConnectorConnectionError;
+				try {
+					connector.Connect ();
+				} catch (Exception ex) {
+					var dialog = new Gtk.MessageDialog (this,
+						Gtk.DialogFlags.DestroyWithParent,
+						Gtk.MessageType.Error,
+						Gtk.ButtonsType.Ok,
+						"Connecting failed! " + ex.Message);
+					dialog.Run ();
+					dialog.Destroy ();
+				}
+
+			};
+			connectDialog.Run ();
+			connectDialog.Destroy ();
+		}
+
+		void OnConnectorConnected(object sender, EventArgs e)
+		{
+			disconnectAction.Sensitive = true;
+			contentvbox.Sensitive = true;
+		}
+
+		void OnConnectorConnectionError(object sender, UnhandledExceptionEventArgs e)
+		{
+			if (e.IsTerminating) {
+				disconnectAction.Sensitive = false;
+				contentvbox.Sensitive = false;
+			}
+			var dialog = new Gtk.MessageDialog (this,
+				             Gtk.DialogFlags.DestroyWithParent,
+				             Gtk.MessageType.Error,
+				             Gtk.ButtonsType.Ok,
+				             "Connection failed! " + ((Exception)e.ExceptionObject).Message);
+			dialog.Run ();
+			dialog.Destroy ();
+		}
+	}
+}
+
